fc6dd96e2b9c089ec8a1c4d7495e4541e76532e0
fideljotojot <fideljotojot@gmail.com>
2026-01-02 23:44:19 +0800

    Add sidebar section with list of Italian regions in HomePage component

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -11,6 +11,17 @@ export default {
 <template>
     <main>
         <header>Welcome, dear {{ user }}</header>
+        <section class="sidebar">
+            <ul>
+                <li>Amalfi and Campania</li>
+                <li>Calabria</li>
+                <li>Liguria</li>
+                <li>Pugulia</li>
+                <li>Sardania</li>
+                <li>Sicilia</li>
+                <li>Toscana</li>
+            </ul>
+        </section>
     </main>
 </template>
 
